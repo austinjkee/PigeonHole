fb55e157509fb44bc922f40a9493a2ac4923c3ec
Teresa Laptop <teresacheung15@gmail.com>
2019-03-30 15:04:44 -0400

    added some widget thing

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from 'react'
 import './App.css'
 import { Button, ButtonToolbar, Table } from 'react-bootstrap';
 import Bar from './containers/Bar';
+import MyFirstGrid from './containers/MyFirstGrid';
 import Info from './containers/Info';
-
 
 const App = () => (
     <div>
         <div className="App">
             <h1>Bar title</h1>
 
-            <Info />
-            <Bar />
+
+            <MyFirstGrid />
         </div>
     </div>
 )
