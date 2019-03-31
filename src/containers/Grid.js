25d18b34454d8784fdac4412c5b3edc90390b248
Teresa Laptop <teresacheung15@gmail.com>
2019-03-31 02:39:19 -0400

    added bar and info charts into the widget with static sizes

--- src/containers/Grid.js
+++ src/containers/Grid.js
@@ -8,10 +8,9 @@ import Info from './Info.js';
 class Grid extends Component {
   render() {
     //https://github.com/STRML/react-grid-layout
-
     var layout = [
       {i: 'a', x: 0, y: 0, w: 11, h: 11.5, minW: 11, maxW: 11, minH: 11.5, maxH: 11.5/*static: true /*static item*/},
-      {i: 'b', x: 11, y: 0, w: 6, h: 6/*, minW: 3, maxW: 6 /*restrict the size of the item with min/max*/},
+      {i: 'b', x: 11, y: 0, w: 6, h: 6, minW: 6, minH: 6 /*restrict the size of the item with min/max*/},
       {i: 'c', x: 17, y: 0, w: 3, h: 3 /*does whatever it wants*/}
     ];
     function handleClick() {
