9dd5d47f29a7ebc82150a58f1d12efddc6c442ed
Austin Kee <austinjkee@gmail.com>
2019-04-02 18:40:56 -0400

    Removed unnecessary references.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import Auth from './Auth';
 import * as serviceWorker from './serviceWorker';
 
