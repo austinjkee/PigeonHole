eee0db76539a5364580e4d106b8c739f7a94b73e
Austin Kee <austinjkee@gmail.com>
2019-04-02 04:28:17 -0400

    Got React working with the input form.

--- src/Auth.js
+++ src/Auth.js
@@ -1,25 +1,67 @@
 import React from 'react'
-import { Button, ButtonToolbar, Table } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import { MySQL } from 'mysql';
+import { Bcrypt } from 'bcryptjs';
 
-const Auth = () => (
-    <form>
-    <div id="text-fields">
-        <br />
-            <input class="login-text" type="text" name="username" placeholder="U S E R N A M E" />
-        <br />
-        <br />
-            <input class="login-text" type="password" name="password" placeholder="P A S S W O R D" />
-        <br />
-    </div>
-    <div id="buttons">
-        <br />
-            <input class="login-button" type="submit" name="uauth-submit" value="S I G N  I N" />
-        <br />
-        <br />
-            <input class="login-button" type="submit" name="uauth-create" value="C R E A T E  A C C O U N T" />
-        <br />
-    </div>
-    </form>
-)
+class Auth extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {uname: '', pword: ''};
 
+    this.handleUnameChange = this.handleUnameChange.bind(this);
+    this.handlePwordChange = this.handlePwordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  handleUnameChange(event) {
+    this.setState({uname: event.target.value});
+  }
+
+  handlePwordChange(event) {
+    this.setState({pword: event.target.value});
+  }
+
+  handleSubmit(event) {
+    if(this.state.uname == '' || this.state.pword == ''){
+        alert('Missing fields!');
+    }
+    else{
+        alert('A name was submitted: ' + this.state.uname + this.state.pword);
+    }
+    event.preventDefault();
+  }
+
+  handleAdd(event) {
+    alert('An entry was added: ' + this.state.uname + this.state.pword);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+        <form onSubmit={this.handleSubmit}>
+        <div id="text-fields">
+            <br />
+                <input class="login-text" type="text" name="username" placeholder="U S E R N A M E" value={this.state.uname} onChange={this.handleUnameChange} />
+            <br />
+            <br />
+                <input class="login-text" type="password" name="password" placeholder="P A S S W O R D" value={this.state.pword} onChange={this.handlePwordChange} />
+            <br />
+        </div>
+        <div id="buttons">
+            <br />
+                <Button variant="outline-light" type="Submit">
+                Sign In
+                </Button>
+            <br />
+            <br />
+                <Button variant="outline-light" onClick={this.handleAdd}>
+                Create Account
+                </Button>
+            <br />
+        </div>
+        </form>
+    );
+  }
+}
 export default Auth
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import Auth from './Auth';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Auth />, document.getElementById('login-form'))
+ReactDOM.render(<Auth />, document.getElementById('login-form'));
 //ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
