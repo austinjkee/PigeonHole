d7215938942eca35f6506b008df0d1ffaea76a29
Austin Kee <austinjkee@gmail.com>
2019-04-19 02:45:16 -0400

    Restored an include in the main app that should not have been removed.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import { Button, ButtonToolbar, Table } from 'react-bootstrap';
 import Bar from './containers/Bar';
