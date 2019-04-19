a8edbf08df8fdc985ec85c5bf272cfabb65914c6
Austin Kee <austinjkee@gmail.com>
2019-04-18 20:07:11 -0400

    Combined twitterApi test content.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
 const express = require('express'),
         mysql = require('mysql'),
         fetch = require('node-fetch');
+const mariadb = express();
+
+const Twitter = require('twitter-lite');
+
+const T = new Twitter({
+    consumer_key: 'Ayl6GGBwJ4m8fhZ7CXWljsgvF',
+    consumer_secret: 'PkVV4LGTKTw1eegLi9DoiJvQ60M9ozdRy2kSrGeYDZupfPACrc',
+    access_token_key: '1105912383346757634-U7nyH07Ajiz3bFyixsIWwaUEa5dX4f',
+    access_token_secret: 'HZHk3jTGzdrHsuqMptfUXgr5T701xLiuztpfh2AQqLcNL'
+});
 
 const PORT = process.env.PORT || 3001;
 
-const mariadb = express();
+
 
 const connection = mysql.createConnection({
     host: 'localhost', //HOSTNAME (localhost)
@@ -24,4 +34,21 @@ require('./routes/html-routes')(mariadb, connection);
 
 mariadb.listen(PORT, () => {
     console.log("App is running on: " + PORT);
+});
+
+mariadb.get('/twitter', (req, res) => {
+
+console.log("twitter has been called");
+
+const twitterData = T
+.get("statuses/show", {
+  id: "1016078154497048576"
+})
+  .then(results => {
+    console.log("results", results);
+    //res.send({ express: results.id });
+    res.json({express: results});
+  })
+  .catch(console.error);
+
 });
