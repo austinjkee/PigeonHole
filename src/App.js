d1e1576d32a9a8d9a928086a1e5230e532248129
Austin Kee <austinjkee@gmail.com>
2019-04-23 18:11:20 -0400

    Fixed a bug with onClick.

--- src/App.js
+++ src/App.js
@@ -493,7 +493,7 @@ class App extends React.Component {
                 <nav className="hidden-ms navbar-light navbar-expand-lg ml-auto">
                     <Container className="nav navbar-nav">
                         <Col className="nav-item">
-                            <a role="button" className="nav-link" onClick={this.handleLogout}>
+                            <a role="button" onClick={this.handleLogout}>
                                 <li>Log Out</li>
                             </a>
                         </Col>
