5341d187882f4f2d298397b7c4cd5452b2ae82a9
Teresa Laptop <teresacheung15@gmail.com>
2019-04-22 22:07:34 -0400

    added individual functions to remove charts/tables
    added individual functions to remove charts/tables

--- src/containers/Grid.js
+++ src/containers/Grid.js
@@ -16,21 +16,46 @@ class Grid extends Component {
       {i: 'b', x: 9.5, y: 0, w: 13, h: 7.5, minW: 13, minH: 7.5 /*restrict the size of the item with min/max*/},
       {i: 'c', x: 9.5, y: 12.5, w: 15, h: 7, minW: 15, minH: 7 /*does whatever it wants*/}
     ];
-    function handleClick() {
-      alert("An element should be added.");
+    function handleClickBar() {
+      //alert("An element should be added.");
+      var x = document.getElementById("myBar");
+      if(x.style.display === "none"){
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+    function handleClickChart1() {
+      //alert("An element should be added.");
+      var x = document.getElementById("chart1");
+      if(x.style.display === "none"){
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+    function handleClickChart2() {
+      //alert("An element should be added.");
+      var x = document.getElementById("chart2");
+      if(x.style.display === "none"){
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
     }
 
     return (
       <div>
-        <Button onClick={handleClick}>Add Graph</Button>
-        <Button onClick={handleClick}>Add Table</Button>
-        <Button onClick={handleClick}>Add Map</Button>
-
+        <div align = "center">
+          <Button onClick={handleClickBar}>Add Graph</Button>
+          <Button onClick={handleClickChart1}>Add Table1</Button>
+          <Button onClick={handleClickChart2}>Add Table2</Button>
+        </div>
         <GridLayout className="layout" layout={layout} cols={24} rowHeight={30} width={1300}>
           {/*sets the size of the grid*/}
-          <div key="a" className="BarBar"><BarBar/></div>
-          <div key="b" className="Info"><Info/></div>
-          <div key="c" className="Info2"><Info2/></div>
+          <div key="a" className="BarBar" id = "myBar"><BarBar/></div>
+          <div key="b" className="Info" id="chart1"><Info/></div>
+          <div key="c" className="Info2" id = "chart2"><Info2/></div>
         </GridLayout>
       </div>
     );
