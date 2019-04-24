fb4fedf48587d8d5f918645f627007433b679218
Austin Kee <austinjkee@gmail.com>
2019-04-24 11:34:34 -0400

    Bar.

--- src/containers/Bar.js
+++ src/containers/Bar.js
@@ -4,6 +4,7 @@ import { generateCountriesData } from '@nivo/generators'
 
 class TrendBar extends React.Component{
     constructor(props) {
+        super(props);
         this.state = {
             data: null
         }
