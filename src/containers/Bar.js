84090c8e2f1863bb207588d7cfbb87e6b700ebb3
Austin Kee <austinjkee@gmail.com>
2019-04-25 09:38:23 -0400

    Tried to make cookie work.

--- src/containers/Bar.js
+++ src/containers/Bar.js
@@ -15,8 +15,8 @@ class Bar extends React.Component{
     }
 
     componentDidMount(){
-        //var tcache = Cookies.get('tcache');
-        //this.setState({data: tcache});
+        var tcache = Cookies.get('tcache');
+        this.setState({data: JSON.parse(tcache)});
     }
 
     shouldComponentUpdate(nextProps) {
