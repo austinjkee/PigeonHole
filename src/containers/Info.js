ae9aebdea129c31165ab3ee8c3f3f23499c4ffca
Austin Kee <austinjkee@gmail.com>
2019-04-25 03:31:21 -0400

    Fixed this refs.

--- src/containers/Info.js
+++ src/containers/Info.js
@@ -1,75 +1,66 @@
 import React, {Component} from "react";
 import { Button, ButtonToolbar, Table } from 'react-bootstrap';
-import Cookies from 'js-cookie';
+import ReactTable from "react-table";
 
-//const Info = () => (
-class Info extends Component {
-    constructor() {
-        super()
-        this.state = {
-            data: null
-        }
+import * as data from '../json/search_university.json';
 
-        this.componentDidMount = this.componentDidMount.bind(this);
-        this.shouldComponentUpdate = this.shouldComponentUpdate.bind(this);
+const stuff = data[0].name;
+console.log(stuff);
+
+class Info extends Component{
+  render(){
+    function hi(){
+      console.log("hi");
     }
-
-    componentDidMount(){
-        var tcache = Cookies.get('tcache');
-        this.setState({data: tcache});
-    }
-
-    shouldComponentUpdate(nextProps) {
-        const differentTrends = this.data !== nextProps.info;
-        return differentTrends;
-    }
-
-    render() {
-      var z = this.props.info
-      let qwerty = "";
-      console.log("object", z);
-      if (z != null)
-      {
-          this.setState({data: this.props.info});
-          console.log("z is not null");
-
-        if (z != null)
-        {
-          this.state.data.sort((a, b) => a.tweet_volume < b.tweet_volume);
-          this.state.data = this.state.data.slice(0, 10);
-          qwerty = this.state.data.map((item, i) => {
-              console.log(item.name);
-                return (
+    return(
+      <div className="Info">
+          <div>
+              <Table className="table">
+                  <thead className="thead-dark">
+                  <tr>
+                      <th scope="col">Name</th>
+                      <th scope="col">Location</th>
+                      <th scope="col">Followers</th>
+                      <th scope="col">Friends</th>
+                  </tr>
+                  </thead>
+                  <tbody>
                     <tr>
-                        <td>{item.name}</td>
-                        <td>{item.tweet_volume}</td>
+                      <th>Twitter University</th>
+                      <th>San Francisco, CA</th>
+                      <th>39260</th>
+                      <th>82</th>
                     </tr>
-
-                );
-              });
-          }
-
-      }
-    return (
-
-    <div>
-        <div className="Info">
-            <table class="table">
-                <thead class="thead-dark">
                     <tr>
-                        <th scope="col">Trend</th>
-                        <th scope="col">Tweet Volume</th>
+                      <th>Columbia University</th>
+                      <th>New York, New York</th>
+                      <th>305118</th>
+                      <th>538</th>
                     </tr>
-                </thead>
-                <tbody>
-                    {qwerty}
-                </tbody>
-            </table>
-
-    </div>
-</div>
-);
-}
+                    <tr>
+                      <th>Stanford University</th>
+                      <th>Stanford, CA</th>
+                      <th>657354</th>
+                      <th>512</th>
+                    </tr>
+                    <tr>
+                      <th>Harvard University</th>
+                      <th>Cambridge, MA</th>
+                      <th>923678</th>
+                      <th>768</th>
+                    </tr>
+                    <tr>
+                      <th>Oxford University</th>
+                      <th>Oxford, UK</th>
+                      <th>499708</th>
+                      <th>989</th>
+                    </tr>
+                  </tbody>
+              </Table>
+          </div>
+      </div>
+    );
+  }
 }
 
 export default Info;
--- src/containers/Table.js
+++ src/containers/Table.js
@@ -1,92 +1,44 @@
-import React, { Component } from 'react';
-import Cookies from 'js-cookie';
+import React, {Component} from "react";
+import { Button, ButtonToolbar, Table } from 'react-bootstrap';
 
-class SearchTable extends Component {
-    constructor() {
-        super();
-        this.state = {
-            data: null,
-        }
+import * as data from '../json/search_university.json';
 
-        this.componentDidMount = this.componentDidMount.bind(this);
-        this.shouldComponentUpdate = this.shouldComponentUpdate.bind(this);
+const stuff = data[0].name;
+console.log(stuff);
+
+class TweetTable extends Component{
+  render(){
+    function hi(){
+      console.log("hi");
     }
-
-    componentDidMount(){
-        var scache = Cookies.get('scache');
-        this.setState({data: scache});
-    }
-
-    shouldComponentUpdate(nextProps) {
-        const differentTrends = this.data !== nextProps.info;
-        return differentTrends;
-    }
-
-  render() {
-      //var q = JSON.stringify(this.props.info);
-      var z = this.props.info
-//this.setState({data: w});
-      //console.log("w", w);
-      //console.log("Asda", w);
-      //var z = JSON.parse(w);
-      let qwerty = "";
-      console.log("object", z);
-      if (z != null)
-      {
-          this.setState({data: this.props.info});
-          console.log("z is not null in search");
-          console.log("object search", z);
-          console.log("object within:", z.statuses);
-
-        if (this.state.data.statuses != null)
-        {
-          qwerty = this.state.data.statuses.map((item, i) => {
-              //console.log(item.name);
-                return (
-                    <tr>
-                        <td>{item.user.name}</td>
-                        <td>{item.full_text}</td>
-                        <td>{item.favorite_count}</td>
-                        <td>{item.retweet_count}</td>
-                    </tr>
-
-
-                );
-              });
-          }
-
-      }
-      //console.log("object2", JSON.stringify(z));
-      // <option key={i} value={item}>
-      //   {item.name}{ item.tweet_volume}
-      // </option>
-      //<th scope="row">1</th>
-      //
-
-    return (
-
-        <div>
-        <div>
-        </div>
+    return(
+      <div>
           <div className="Info">
-              <table class="table">
-                  <thead class="thead-dark">
-                      <tr>
-                          <th scope="col">Name</th>
-                          <th scope="col">Tweet</th>
-                          <th scope="col">Favorite Count</th>
-                          <th scope="col">Retweet Count</th>
-                      </tr>
+              <table className="table">
+                  <thead className="thead-dark">
+                  <tr>
+                      <th scope="col">Name</th>
+                      <th scope="col">Tweet</th>
+                      <th scope="col">Time</th>
+                  </tr>
                   </thead>
                   <tbody>
-                  {qwerty}
+                    <tr>
+                      <th>Twitter University</th>
+                      <th>SRT @ElliottSaslow: TWITTER ELECTION INTEGRITY DATASETS\n\nWe are very lucky @galvanize to be hosting @Twitter\u2019s own @ivantures discussing the\u2026</th>
+                      <th>Fri Feb 22 22:45:04 +0000 2019</th>
+                    </tr>
+                    <tr>
+                      <th>Columbia University</th>
+                      <th>RT @Columbia_Biz: Tomorrow\u2019s business leaders are today\u2019s MBAs. This week on @ColumbiaBizcast: Why Dean Glenn Hubbard believes \"we\u2019re in ex\u2026</th>
+                      <th>Tue Feb 26 17:54:54 +0000 2019</th>
+                    </tr>
                   </tbody>
-
               </table>
-
+          </div>
       </div>
-  </div>
     );
   }
- }
- export default SearchTable;
+}
+
+export default TweetTable;
