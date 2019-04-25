d13f037e54f176f4b592797a3dcf47f2df10efcf
Austin Kee <austinjkee@gmail.com>
2019-04-25 14:05:10 -0400

    Changed the way it updates.

--- src/App.js
+++ src/App.js
@@ -295,7 +295,7 @@ class App extends React.Component {
       //if(Cookies.get('scache') === undefined){
       this.callBackendAPI3()
         .then(function(res) {
-            this.setState({ search: JSON.stringify(res) });
+            this.setState({ search: res });
         })
         .catch(err => console.log(err));
     //}
