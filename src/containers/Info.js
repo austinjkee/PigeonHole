f70a69191ef0be485edcf30b00c1d14afe3dd038
Austin Kee <austinjkee@gmail.com>
2019-04-25 14:28:26 -0400

    Changed the way it updates.

--- src/containers/Info.js
+++ src/containers/Info.js
@@ -10,83 +10,15 @@ class Info extends React.Component{
             data: [],
         }
 
-        this.componentDidMount = this.componentDidMount.bind(this);
-        this.componentDidUpdate = this.componentDidUpdate.bind(this);
-    }
-
-    componentDidMount(){
-        var tcache = Cookies.get('dcache');
-        if(tcache !== undefined){
-            this.setState({data: JSON.parse(tcache)}, function(){
-                console.log("data", this.state.data);
-            });
-        }
-        else{
-            var z = this.props.info;
-
-            //var z = JSON.parse(w);
-            console.log("The Data Passed To Bar",z);
-            let qwerty = "";
-            //console.log("object", z);
-
-            if (z != null)
-            {
-                console.log("z is not null");
-                console.log("object345", z);
-                //this.setState({data: z});
-
-                Cookies.set('dcache', JSON.stringify(z), { maxAge: 90000 });
-                console.log("Lots of Stuff", JSON.stringify(z));
-
-                  //console.log(qwerty);
-                // qwerty = z.trends[0].trends.map((item, i) => {
-                //     console.log(item.name);
-                //       return (
-                //           <tr>
-                //               <td>{item.name}</td>
-                //               <td>{item.tweet_volume}</td>
-                //           </tr>
-                //
-                //       );
-                //     });
-            }
-        }
-    }
-
-    componentDidUpdate(){
-        var z = this.props.info;
-
-        //var z = JSON.parse(w);
-        console.log("The Data Passed To Bar",z);
-        let qwerty = "";
-        //console.log("object", z);
-
-        if (z != null)
-        {
-            console.log("z is not null");
-            console.log("object345", z);
-            Cookies.set('dcache', JSON.stringify(z), { maxAge: 90000 });
-
-              //console.log(qwerty);
-            // qwerty = z.trends[0].trends.map((item, i) => {
-            //     console.log(item.name);
-            //       return (
-            //           <tr>
-            //               <td>{item.name}</td>
-            //               <td>{item.tweet_volume}</td>
-            //           </tr>
-            //
-            //       );
-            //     });
-        }
     }
 
     render(){
-        var qwerty = [];
-        var tcache = Cookies.get('dcache');
+        var qwerty = "";
+        var tcache = Cookies.get('tcache');
         if(tcache !== undefined){
-            qwerty = JSON.parse(tcache).map((item, i) => {
-            //console.log(item.name);
+            var stcache = JSON.parse(tcache);
+            qwerty = stcache.map((item, i) => {
+            console.log(item.name);
               return (
                   <tr>
                       <td>{item.name}</td>
@@ -98,7 +30,7 @@ class Info extends React.Component{
         }
         else{
             qwerty = this.state.data.map((item, i) => {
-            //console.log(item.name);
+            console.log(item.name);
               return (
                   <tr>
                       <td>{item.name}</td>
