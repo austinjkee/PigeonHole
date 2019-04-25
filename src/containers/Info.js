f57419b120ee90573ffb60d6d46c7f7db023cc3d
Austin Kee <austinjkee@gmail.com>
2019-04-25 13:51:01 -0400

    Changed the way it updates.

--- src/containers/Info.js
+++ src/containers/Info.js
@@ -1,66 +1,151 @@
-import React, {Component} from "react";
-import { Button, ButtonToolbar, Table } from 'react-bootstrap';
-import ReactTable from "react-table";
+import React from 'react';
+import { ResponsiveBar } from '@nivo/bar';
+import { generateCountriesData } from '@nivo/generators';
+import Cookies from 'js-cookie';
 
-import * as data from '../json/search_university.json';
+class Info extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+            data: [],
+        }
 
-const stuff = data[0].name;
-console.log(stuff);
-
-class Info extends Component{
-  render(){
-    function hi(){
-      console.log("hi");
+        this.componentDidMount = this.componentDidMount.bind(this);
+        this.componentDidUpdate = this.componentDidUpdate.bind(this);
     }
-    return(
-      <div className="Info">
+
+    componentDidMount(){
+        var tcache = Cookies.get('dcache');
+        if(tcache !== undefined){
+            this.setState({data: JSON.parse(tcache)}, function(){
+                console.log("data", this.state.data);
+            });
+        }
+        else{
+            var z = this.props.info;
+
+            //var z = JSON.parse(w);
+            console.log("The Data Passed To Bar",z);
+            let qwerty = "";
+            //console.log("object", z);
+
+            if (z != null)
+            {
+                console.log("z is not null");
+                console.log("object345", z);
+                //this.setState({data: z});
+
+                if (z != null)
+                {
+                    qwerty = z.map((item, i) => {
+                        //console.log(item.name);
+                        return (
+                            <tr>
+                                <td>{item.name}</td>
+                                <td>{item.tweet_volume}</td>
+                            </tr>
+
+                        );
+                    });
+                    Cookies.set('dcache', z, { maxAge: 90000 });
+                    console.log("Lots of Stuff", w);
+
+                }
+
+                  //console.log(qwerty);
+                // qwerty = z.trends[0].trends.map((item, i) => {
+                //     console.log(item.name);
+                //       return (
+                //           <tr>
+                //               <td>{item.name}</td>
+                //               <td>{item.tweet_volume}</td>
+                //           </tr>
+                //
+                //       );
+                //     });
+            }
+        }
+    }
+
+    componentDidUpdate(){
+        var z = this.props.info;
+
+        //var z = JSON.parse(w);
+        console.log("The Data Passed To Bar",z);
+        let qwerty = "";
+        //console.log("object", z);
+
+        if (z != null)
+        {
+            console.log("z is not null");
+          console.log("object345", z);
+          //this.setState({data: z});
+
+          if (z.trends[0].trends != null)
+          {
+              qwerty = z.map((item, i) => {
+              //console.log(item.name);
+                return (
+                    <tr>
+                        <td>{item.name}</td>
+                        <td>{item.tweet_volume}</td>
+                    </tr>
+
+                );
+              });
+          }
+
+
+            Cookies.set('dcache', qwerty, { maxAge: 90000 });
+            console.log("Lots of Stuff", qwerty);
+
+              //console.log(qwerty);
+            // qwerty = z.trends[0].trends.map((item, i) => {
+            //     console.log(item.name);
+            //       return (
+            //           <tr>
+            //               <td>{item.name}</td>
+            //               <td>{item.tweet_volume}</td>
+            //           </tr>
+            //
+            //       );
+            //     });
+        }
+    }
+
+    render(){
+        var qwerty = [];
+        this.componentDidUpdate();
+        var tcache = Cookies.get('dcache');
+        if(tcache !== undefined){
+            qwery = tcache;
+        }
+        else{
+            qwerty = this.state.data;
+        }
+      return(
           <div>
-              <Table className="table">
-                  <thead className="thead-dark">
-                  <tr>
-                      <th scope="col">Name</th>
-                      <th scope="col">Location</th>
-                      <th scope="col">Followers</th>
-                      <th scope="col">Friends</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <th>Twitter University</th>
-                      <th>San Francisco, CA</th>
-                      <th>39260</th>
-                      <th>82</th>
-                    </tr>
-                    <tr>
-                      <th>Columbia University</th>
-                      <th>New York, New York</th>
-                      <th>305118</th>
-                      <th>538</th>
-                    </tr>
-                    <tr>
-                      <th>Stanford University</th>
-                      <th>Stanford, CA</th>
-                      <th>657354</th>
-                      <th>512</th>
-                    </tr>
-                    <tr>
-                      <th>Harvard University</th>
-                      <th>Cambridge, MA</th>
-                      <th>923678</th>
-                      <th>768</th>
-                    </tr>
-                    <tr>
-                      <th>Oxford University</th>
-                      <th>Oxford, UK</th>
-                      <th>499708</th>
-                      <th>989</th>
-                    </tr>
-                  </tbody>
-              </Table>
-          </div>
-      </div>
-    );
-  }
+           <div>
+           </div>
+             <div className="Info">
+                 <table class="table">
+                     <thead class="thead-dark">
+                         <tr>
+                             <th scope="col">Trend</th>
+                             <th scope="col">Tweet Volume</th>
+
+                         </tr>
+                     </thead>
+                     <tbody>
+                     {qwerty}
+                     </tbody>
+
+                 </table>
+
+         </div>
+     </div>
+  );
+}
 }
 
-export default Info;
+export default Info
