885aef597dc55c6a26b586b0d48db15ed2b12237
Austin Kee <austinjkee@gmail.com>
2019-04-25 02:42:10 -0400

    Tried to fix bar.

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Bar from './containers/Bar';
 import Info from './containers/Info';
 import WTable from './containers/Table';
 import './css/gridLayoutStyle.css';
-import './css/resizeableStyle.css';
+import './css/resizableStyle.css';
 
 const bcrypt = require('bcryptjs'),
      fetch = require('node-fetch');
