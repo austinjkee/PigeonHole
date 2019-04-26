8bfd5e4294e5c9a82dd77cd7dba1889058b2af90
Austin Kee <austinjkee@gmail.com>
2019-04-26 00:58:10 -0400

    Changed the way the cookie is set.

--- src/containers/Table.js
+++ src/containers/Table.js
@@ -13,13 +13,15 @@ class TweetTable extends Component {
     }
 
     componentDidMount() {
+        const z = this.props.info;
         console.log("Search Mounted.", this.props.info);
-        Cookies.set('search', this.props.info, { maxAge: 90000 });
+        Cookies.set('search', z, { maxAge: 90000 });
     }
 
     componentDidUpdate() {
+        const z = this.props.info;
         console.log("Search Updated.", this.props.info);
-        Cookies.set('search', this.props.info, { maxAge: 90000 });
+        Cookies.set('search', z, { maxAge: 90000 });
     }
 
   render() {
